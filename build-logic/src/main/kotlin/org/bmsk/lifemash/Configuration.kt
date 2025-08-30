4382b608f91273445203041bc05892bf281346ee
yibeomseok <vv99911@gmail.com>
2025-08-30 13:26:23 +0900

    refactor: app 모듈 정리

---
+++ build-logic/src/main/kotlin/org/bmsk/lifemash/Configuration.kt
@@ -0,0 +1,14 @@
+package org.bmsk.lifemash
+
+object Configuration {
+    const val applicationId = "org.bmsk.lifemash"
+    const val compileSdk = 35
+    const val targetSdk = 35
+    const val minSdk = 28
+    const val majorVersion = 1
+    const val minorVersion = 3
+    const val patchVersion = 0
+    const val versionName = "$majorVersion.$minorVersion.$patchVersion"
+    const val versionCode = 10
+    const val composeCompilerExtensionVersion = "1.5.9"
+}
