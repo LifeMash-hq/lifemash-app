5897ee802a63aa876328ea6f1785ba445dcc2a2e
yibeomseok <vv99911@gmail.com>
2025-08-30 11:39:10 +0900

    add: data 모듈 공통 로직 추가

--- data/core/build.gradle.kts
+++ data/core/build.gradle.kts
@@ -1,12 +1,7 @@
 plugins {
     id("lifemash.android.library")
-    id("lifemash.android.hilt")
 }
 
 android {
     namespace = "org.bmsk.lifemash.data.core"
-}
-
-dependencies {
-    // Core data dependencies can be added here later
 }
