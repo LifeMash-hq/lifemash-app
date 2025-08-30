88fc7a2d9b6a4fa515f2a78f2c307bb6f894232c
yibeomseok <vv99911@gmail.com>
2025-08-30 13:26:23 +0900

    refactor: app 모듈 정리

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,42 +1,8 @@
-import org.bmsk.buildsrc.Configuration
-
 plugins {
     id("lifemash.android.application")
-    id("com.google.android.gms.oss-licenses-plugin")
+    id("lifemash.android.hilt")
     id("com.google.gms.google-services")
     id("com.google.firebase.crashlytics")
-}
-
-android {
-    namespace = "org.bmsk.lifemash"
-    compileSdk = Configuration.compileSdk
-
-    defaultConfig {
-        applicationId = Configuration.applicationId
-        minSdk = Configuration.minSdk
-        targetSdk = Configuration.targetSdk
-        versionCode = Configuration.versionCode
-        versionName = Configuration.versionName
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = true
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro",
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
 }
 
 dependencies {
