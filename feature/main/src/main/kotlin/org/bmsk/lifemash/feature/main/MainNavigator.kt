12157f5ce08afceb7bd603119632acaeef100dbe
yibeomseok <vv99911@gmail.com>
2025-08-30 15:08:45 +0900

    refactor: MainActivity 코드 정리

--- feature/main/src/main/kotlin/org/bmsk/lifemash/feature/main/MainNavigator.kt
+++ feature/main/src/main/kotlin/org/bmsk/lifemash/feature/main/MainNavigator.kt
@@ -1,24 +1,17 @@
 package org.bmsk.lifemash.feature.main
 
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
 import org.bmsk.lifemash.feature.feed.api.FeedNavController
 import org.bmsk.lifemash.feature.scrap.api.ScrapNavController
 import org.bmsk.lifemash.feature.topic.api.WebViewNavController
 import org.bmsk.lifemash.feature.topic.api.WebViewNavControllerInfo
 
 internal class MainNavigator(
+    private val navController: NavHostController,
     private val scrapNavController: ScrapNavController,
     private val webViewNavController: WebViewNavController,
     private val feedNavController: FeedNavController,
-    val navController: NavHostController,
 ) {
-//    private val currentDestination: NavDestination?
-//        @Composable get() = navController
-//            .currentBackStackEntryAsState().value?.destination
-
     val startDestination = feedNavController.route()
 
     fun navigateWebView(url: String) {
@@ -32,19 +25,4 @@ internal class MainNavigator(
     fun navigateFeed() {
         feedNavController.navigate(navController, Unit)
     }
-}
-
-@Composable
-internal fun rememberMainNavigator(
-    scrapNavController: ScrapNavController,
-    webViewNavController: WebViewNavController,
-    feedNavController: FeedNavController,
-    navController: NavHostController = rememberNavController(),
-): MainNavigator = remember(navController) {
-    MainNavigator(
-        scrapNavController = scrapNavController,
-        webViewNavController = webViewNavController,
-        feedNavController = feedNavController,
-        navController = navController
-    )
 }
