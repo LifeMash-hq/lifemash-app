f11765e9413a8e9ed8909f80cc2b52afcd8a7073
yibeomseok <vv99911@gmail.com>
2025-09-07 19:51:50 +0900

    refactor(domain): UseCase에 Hilt 적용 및 의존성 주입 리팩터링
    - 기존 Dagger를 사용하던 방식에서 Hilt를 사용하도록 변경
    - UseCaseModule에서 @Provides 대신 @Binds를 사용하여 의존성 주입
    - 각 UseCase 구현체에 @Inject constructor 추가
    - app 모듈의 DomainBridgeModule 제거

--- domain/feature-scrap/src/main/kotlin/org/bmsk/lifemash/domain/scrap/usecase/GetScrappedArticlesUseCase.kt
+++ domain/feature-scrap/src/main/kotlin/org/bmsk/lifemash/domain/scrap/usecase/GetScrappedArticlesUseCase.kt
@@ -3,12 +3,13 @@ package org.bmsk.lifemash.domain.scrap.usecase
 import kotlinx.coroutines.flow.Flow
 import org.bmsk.lifemash.domain.core.model.Article
 import org.bmsk.lifemash.domain.scrap.repository.ScrapRepository
+import javax.inject.Inject
 
 interface GetScrappedArticlesUseCase {
     operator fun invoke(): Flow<List<Article>>
 }
 
-internal class GetScrappedArticlesUseCaseImpl(
+internal class GetScrappedArticlesUseCaseImpl @Inject constructor(
     private val scrapRepository: ScrapRepository
 ) : GetScrappedArticlesUseCase {
     override operator fun invoke(): Flow<List<Article>> {
