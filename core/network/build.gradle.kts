679ee96ec7dd445f72f47a886b00c30b849eaed0
yibeomseok <vv99911@gmail.com>
2026-03-14 16:40:21 +0900

    refactor: Algolia 의존성 완전 제거
    Algolia SDK, 관련 서비스 파일, 테스트, Gradle 의존성을 모두 제거.
    검색은 Firestore 기반 searchArticles Cloud Function으로 대체 완료.
    
    - LifeMashAlgoliaService.kt 삭제
    - AlgoliaClientProvider.kt 삭제
    - LifeMashAlgoliaServiceTest.kt 삭제
    - libs.versions.toml에서 algolia, ktor 항목 제거
    - build.gradle.kts에서 algolia 의존성 제거
    - LifeMashFirebaseServiceTest.kt에서 Algolia import 제거

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -32,9 +32,6 @@ dependencies {
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.firestore)
     implementation(libs.firebase.functions)
-    implementation(platform(libs.algolia.bom))
-    implementation(libs.algolia.client)
-    implementation(libs.ktor.client.okhttp)
 
     androidTestImplementation(libs.androidx.test.runner)
     androidTestImplementation(libs.androidx.test.ext)
