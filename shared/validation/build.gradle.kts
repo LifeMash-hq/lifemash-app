3120582e947a5a98f9e1fd4582c3d37fca7afad5
yibeomseok <vv99911@gmail.com>
2026-03-26 18:07:19 +0900

    CI 브랜치 트리거에 develop-2026 추가, shared:validation maven-publish 설정

--- shared/validation/build.gradle.kts
+++ shared/validation/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     id("lifemash.kmp.library")
+    `maven-publish`
 }
+
+group = "org.bmsk.lifemash"
+version = "0.1.0"
 
 kotlin {
     android {
@@ -13,6 +17,19 @@ kotlin {
         commonMain.dependencies { }
         commonTest.dependencies {
             implementation(kotlin("test"))
+        }
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/LifeMash-hq/lifemash-app")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR") ?: ""
+                password = System.getenv("GITHUB_PACKAGES_TOKEN") ?: System.getenv("GITHUB_TOKEN") ?: ""
+            }
         }
     }
 }
