e20cd9a12cf990ae9c127ddfe2adf7387ac5d2fb
yibeomseok <vv99911@gmail.com>
2026-03-27 19:14:16 +0900

    separate kakao key and backend url by build type

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,15 +7,14 @@ plugins {
 }
 
 val localProps = gradleLocalProperties(rootDir, providers)
-val kakaoNativeAppKey: String = localProps.getProperty("KAKAO_NATIVE_APP_KEY", "")
-val debugBackendUrl: String = localProps.getProperty("BACKEND_BASE_URL", "http://10.0.2.2:8080")
-val releaseBackendUrl = "https://lifemash-backend.onrender.com"
+
+val kakaoKeyDebug = localProps.getProperty("KAKAO_NATIVE_APP_KEY_DEBUG", "")
+val kakaoKeyRelease = localProps.getProperty("KAKAO_NATIVE_APP_KEY_RELEASE", "")
+val debugBackendUrl = localProps.getProperty("BACKEND_BASE_URL_DEBUG", "http://10.0.2.2:8080")
+val releaseBackendUrl = localProps.getProperty("BACKEND_BASE_URL_RELEASE", "https://lifemash-backend.onrender.com")
 
 android {
-    defaultConfig {
-        manifestPlaceholders["KAKAO_NATIVE_APP_KEY"] = kakaoNativeAppKey
-        buildConfigField("String", "KAKAO_NATIVE_APP_KEY", "\"$kakaoNativeAppKey\"")
-    }
+    defaultConfig { }
     signingConfigs {
         create("release") {
             val storeFilePath = localProps.getProperty("RELEASE_STORE_FILE", "")
@@ -30,10 +29,14 @@ android {
     buildTypes {
         debug {
             applicationIdSuffix = ".debug"
+            manifestPlaceholders["KAKAO_NATIVE_APP_KEY"] = kakaoKeyDebug
+            buildConfigField("String", "KAKAO_NATIVE_APP_KEY", "\"$kakaoKeyDebug\"")
             buildConfigField("String", "BACKEND_BASE_URL", "\"$debugBackendUrl\"")
         }
         release {
             signingConfig = signingConfigs.getByName("release")
+            manifestPlaceholders["KAKAO_NATIVE_APP_KEY"] = kakaoKeyRelease
+            buildConfigField("String", "KAKAO_NATIVE_APP_KEY", "\"$kakaoKeyRelease\"")
             buildConfigField("String", "BACKEND_BASE_URL", "\"$releaseBackendUrl\"")
         }
     }
